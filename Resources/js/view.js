8479f650860b9862a9a47ad61105af9dc40fc545
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-11-15 16:33:49 -0500

    add shop window

--- Resources/js/view.js
+++ Resources/js/view.js
@@ -1,22 +1,84 @@
-YUI().use('view', function(Y) { 
+YUI().use('view','panel', function(Y) { 
 ///start
+    
+
+var createAddShopPanel = function() {
+
+    var panel = new Y.Panel({
+        srcNode: '#add-shop-panel',
+        width: 250, 
+        centered: true,
+        visible: false,
+        modal: true,
+        headerContent: 'Add A New Shop',
+        zIndex: 10
+    });
+    
+    panel.addButton({
+        value: 'Add Shop',
+        action: function(e) {
+            e.preventDefault(); 
+
+            console.log('Add a new shop submit!');
+            console.log(e);
+        },
+        section: Y.WidgetStdMod.FOOTER
+    });
+
+    panel.addButton({
+        value: 'Cancel',
+        action: function(e) {
+            e.preventDefault(); 
+            panel.hide();
+        },
+        section: Y.WidgetStdMod.FOOTER
+    });
+
+    panel.render();
+    Y.one('#add-shop-panel').removeClass('util-hide');
+
+    return panel;
+};
+
 
 Themer.appView = Y.Base.create('appView', Y.View, [], {
    
-   container: Y.one('#container'),
+    container: Y.one('#container'),
 
-   events: {
-       '#add-shop' : { click: 'addShop'}
-   },
+    events: {
+        '#add-shop' : { click: 'addShop'}
+    },
    
-   initializer: function() {
-       //Set up app!
-       console.log('Initializer appView');
-   },
+    initializer: function() {
+        console.log('Initializer appView');
+
+        //Setup the Add Shop form overlay
+        this.addShopForm = createAddShopPanel();
+        // this.addShopForm.render();
+
+        var shops = this.shops = new Themer.shopList();
+
+        shops.after('add', this.add, this);
+        shops.after('remove', this.remove, this);
+
+        shops.load();
+
+    },
    
-   addShop: function(e) {
-       console.log('Add Shop!');
-   }
+    //Click handler for the add shop button
+    addShop: function(e) {
+        console.log('Add Shop!');
+        this.addShopForm.show();
+    }, 
+
+    add: function(e) {
+        console.log('New Shop Added');
+    },
+
+    remove: function(e) {
+        console.log('Shop Removed');
+    }
+
 });
 
 ///end
