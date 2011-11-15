0c984ab49ece2e48c2e5ff246269940c75677f83
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-11-15 16:40:23 -0500

    Show onboard if no shops

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -1,9 +1,13 @@
 YUI().use('event', function(Y) {Y.on("domready", function() { 
     console.log('Dom Ready');
 
-    new Themer.appView();
-    //Load shops
+    var app = new Themer.appView();
+
     //No shop? Show starting instructions
+    if(app.shops.isEmpty()) {
+        console.log('No Shops! Show Onboard!');
+        Y.one('#onboard').removeClass('util-hide');
+    }
 
 });});
 
