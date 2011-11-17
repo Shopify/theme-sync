0ca9c45d80af04637b46f6f473d56efa7fc9edb0
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-11-17 01:11:17 -0500

    moving render code into render

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -1,13 +1,4 @@
-YUI().use('event', function(Y) {Y.on("domready", function() { 
-    console.log('Dom Ready');
-
-    var app = new Themer.appView();
-
-    //No shop? Show starting instructions
-    if(app.shops.isEmpty()) {
-        console.log('No Shops! Show Onboard!');
-        Y.one('#onboard').removeClass('util-hide');
-    }
-
+YUI().use('event', 'event-focus', function(Y) {Y.on("domready", function() { 
+    new Themer.appView();
 });});
 
