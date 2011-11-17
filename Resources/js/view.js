7248e90ef77d6f98be26fb962562afbab0e2865c
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-11-17 01:11:24 -0500

    getting render working

--- Resources/js/view.js
+++ Resources/js/view.js
@@ -1,4 +1,4 @@
-YUI().use('view','panel', function(Y) { 
+YUI().use('view','panel', 'event-custom','event-focus', function(Y) { 
 ///start
 
 Themer.appView = Y.Base.create('appView', Y.View, [], {
@@ -14,23 +14,55 @@ Themer.appView = Y.Base.create('appView', Y.View, [], {
 
         //Setup the Add Shop form overlay
         this.addShopForm = createAddShopPanel();
-        // this.addShopForm.render();
 
         var shops = this.shops = new Themer.shopList();
-
         shops.after('add', this.add, this);
-        shops.after('remove', this.remove, this);
+        shops.after('remove', this.remove, this);        
+
+        //Reset also fires on initial model list load. 
+        shops.after('reset', this.render, this);
 
         shops.load();
 
+        //Custom event that passes in the data
+        Y.on('addShopOk', function(shopData) {
+            console.log('Add Shop OK!');
+            this.shops.create(shopData);
+        }, this);
+
+    },
+
+    render: function(e) {
+        console.log('RENDER: appView');
+
+        if(this.shops.isEmpty()) {
+            console.log('No Shops! Show Onboard!');
+            Y.one('#onboard').removeClass('util-hide');
+            return this;
+        }
+
+        Y.one('#onboard').addClass('util-hide');
+
+        var fragment = Y.one(Y.config.doc.createDocumentFragment());
+
+        Y.Array.each(e.models, function (model) {
+            console.log(model);
+            var view = new ShopView({model: model});
+            fragment.append(view.render().container);
+        });
+
+        this.container.one('#content').setContent(fragment);
+
+        return this;
     },
    
     //Click handler for the add shop button
     addShop: function(e) {
-        console.log('Add Shop!');
+        console.log('addd');
         this.addShopForm.show();
     }, 
 
+    //Called when something added to the list.
     add: function(e) {
         console.log('New Shop Added');
     },
@@ -45,7 +77,7 @@ var createAddShopPanel = function() {
 
     var panel = new Y.Panel({
         srcNode: '#add-shop-panel',
-        width: 250, 
+        width: 400, 
         centered: true,
         visible: false,
         modal: true,
@@ -54,12 +86,24 @@ var createAddShopPanel = function() {
     });
     
     panel.addButton({
+        id: 'addShopOk',
         value: 'Add Shop',
         action: function(e) {
+            console.log('Add a new shop submit!');
+            // console.log(e);
+
             e.preventDefault(); 
 
-            console.log('Add a new shop submit!');
-            console.log(e);
+            var data = {
+                id: Y.one('input[name=id]').get('value'),
+                api_key: Y.one('input[name=api_key]').get('value'),
+                password: Y.one('input[name=password]').get('value')
+            };
+            
+            //@todo validate data
+            //Assuming its ok...
+            Y.fire('addShopOk', data);
+            panel.hide();
         },
         section: Y.WidgetStdMod.FOOTER
     });
