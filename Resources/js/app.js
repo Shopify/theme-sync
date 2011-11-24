80ad39c417896d8327195bafd9adea1788d985b8
Mitchell Amihod <mitchell.amihod@gmail.com>
2011-11-24 11:56:49 -0500

    Stop right click working outside of LIs

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -1,4 +1,13 @@
 YUI().use('event', 'event-focus', function(Y) {Y.on("domready", function() { 
     new Themer.appView();
+
+    //Stop right click outside of the LIs
+    //LI contextmenu listener setup in view.js
+    Y.one('body').on('contextmenu', function(e) {
+        e.preventDefault();
+        //Remove contextual right click options
+        // var emptyMenu = Ti.UI.createMenu();
+        // Ti.UI.setContextMenu(emptyMenu);
+    });
 });});
 
