40a4aeadf9f4a79dcf1b5f634c07bd6fd16da5e1
Mitchell Amihod <mitchell.amihod@gmail.com>
2012-01-12 00:19:47 -0300

    move some stuff around.

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -2,28 +2,10 @@ YUI().use('event', 'event-focus','event-custom', 'querystring-parse', function(Y
 //@todo need to spin down watchers when going for remote auth
     
     var theApp = new Themer.appView();
-
-    //Stop right click outside of the LIs
-    //LI contextmenu listener setup in view.js
-    Y.one('body').on('contextmenu', function(e) {
-        // e.preventDefault();
-        //Remove contextual right click options
-        // var emptyMenu = Ti.UI.createMenu();
-        // Ti.UI.setContextMenu(emptyMenu);
-    });
     
     //Spin up watchers...
     Themer.Watcher.init(theApp);
 
-    Y.Global.on('asset:send', function(e) {
-        console.log('asset:send listener');
-        //@todo allowed list?
-        var key = e.relative,
-            path = e.base.concat(Ti.Filesystem.getSeparator(), e.relative);
-
-        Themer.IO.sendAsset(e.shop, e.theme, key, path);
-    });
-    
     //Returning from the callback...
     if(window.location.search != '') {
         var qs = Y.QueryString.parse(window.location.search.replace('?', ''));
@@ -42,6 +24,25 @@ YUI().use('event', 'event-focus','event-custom', 'querystring-parse', function(Y
             }
         });
     }
+
+    Y.Global.on('asset:send', function(e) {
+        console.log('asset:send listener');
+        //@todo allowed list?
+        var key = e.relative,
+            path = e.base.concat(Ti.Filesystem.getSeparator(), e.relative);
+
+        Themer.IO.sendAsset(e.shop, e.theme, key, path);
+    });
+
+    //Stop right click outside of the LIs
+    //LI contextmenu listener setup in view.js
+    Y.one('body').on('contextmenu', function(e) {
+        e.preventDefault();
+        //Remove contextual right click options
+        // var emptyMenu = Ti.UI.createMenu();
+        // Ti.UI.setContextMenu(emptyMenu);
+    });
+
     
 });});
 
