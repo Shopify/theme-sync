551e1942a35e32015da7f201c5d75028c555ccaf
Mitchell Amihod <mitchell.amihod@gmail.com>
2012-01-11 23:35:21 -0300

    Fix behaviour when we come back from callback

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -1,28 +1,12 @@
 YUI().use('event', 'event-focus','event-custom', 'querystring-parse', function(Y) {Y.on("domready", function() { 
-//@todo / bookmark : fix it so when you add first shop, that we don't see
-//                  the You have no shops so add one overlay. Basically a timing issue I suspect
-//                  of when we check
 //@todo need to spin down watchers when going for remote auth
-    if(window.location.search != '') {
-        var shoplist = new Themer.shopList();
-        shoplist.load();
-
-        var qs = Y.QueryString.parse(window.location.search.replace('?', ''));
-        console.log(qs);
-
-        shoplist.create({
-            id:qs.shop.replace('.myshopify.com', '', 'i'),
-            api_key: APP_API_KEY,
-            password: Titanium.Codec.digestToHex(Titanium.Codec.MD5, SHARED_SECRET+qs.t)         
-        });
-    }
     
     var theApp = new Themer.appView();
 
     //Stop right click outside of the LIs
     //LI contextmenu listener setup in view.js
     Y.one('body').on('contextmenu', function(e) {
-        e.preventDefault();
+        // e.preventDefault();
         //Remove contextual right click options
         // var emptyMenu = Ti.UI.createMenu();
         // Ti.UI.setContextMenu(emptyMenu);
@@ -38,6 +22,26 @@ YUI().use('event', 'event-focus','event-custom', 'querystring-parse', function(Y
             path = e.base.concat(Ti.Filesystem.getSeparator(), e.relative);
 
         Themer.IO.sendAsset(e.shop, e.theme, key, path);
-    });    
+    });
+    
+    //Returning from the callback...
+    if(window.location.search != '') {
+        var qs = Y.QueryString.parse(window.location.search.replace('?', ''));
+
+        theApp.shops.create({
+            id:qs.shop.replace('.myshopify.com', '', 'i'),
+            api_key: APP_API_KEY,
+            password: Titanium.Codec.digestToHex(Titanium.Codec.MD5, SHARED_SECRET+qs.t)         
+        }, function(err) {
+            //Hack hack hack!
+            if(!err) {
+                theApp.addShopForm.hide();
+            } else {
+                //@todo Throw up error panel.
+                alert(err);
+            }
+        });
+    }
+    
 });});
 
