437ab37a266f876c2a6490e087d9b04c1531c8d0
Mitchell Amihod <mitchell.amihod@gmail.com>
2012-09-11 20:26:26 -0400

    tried new pattern, higher latency. Working with new version of gem code.

--- test/watch-test.rb
+++ test/watch-test.rb
@@ -4,20 +4,24 @@
 #There is room for improvement in this script. critique welcomed.
 require 'socket'
 require 'rubygems'
-# require 'json'
+require 'json'
 # Listen needs gem install --version '~> 0.9.1' rb-fsevent
 require 'listen'
 
-path = '/Users/mitch/Sites/klocko'
+path = '/Users/mitch/Sites/klocko-and-sons3230-1984692'
 
-# default ignores git, svn, .DS_Store
-Listen.to(path) do |modified, added, removed|
+callback = Proc.new do |modified, added, removed|
   puts 'Modified:'
   puts modified.inspect
   # puts 'Added:'
   # puts added.inspect
 end
 
+
+listener = Listen.to(path)
+listener.change(&callback) # convert the callback to a block and register it
+listener.latency(1)
+listener.start # blocks execution
 
 # FSSM.monitor path do |m|
 #     m.update do |base, relative|
