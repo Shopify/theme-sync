2ab9fe1e1c506200ac5f34697046ce8ccff36da1
Mitchell Amihod <mitchell.amihod@gmail.com>
2012-09-12 22:58:53 -0400

    test of listen working

--- test/watch-test.rb
+++ test/watch-test.rb
@@ -1,48 +1,32 @@
 #!/usr/bin/ruby
-#opened bug https://github.com/guard/listen/issues/61
-#
-#There is room for improvement in this script. critique welcomed.
+
 require 'socket'
 require 'rubygems'
 require 'json'
-# Listen needs gem install --version '~> 0.9.1' rb-fsevent
 require 'listen'
 
-path = '/Users/mitch/Sites/klocko-and-sons3230-1984692'
+base_path = '/Users/mitch/Sites/klocko-and-sons3230-1984692'
 
-callback = Proc.new do |modified, added, removed|
-  puts 'Modified:'
-  puts modified.inspect
-  # puts 'Added:'
-  # puts added.inspect
+def broadcast_assets(assets, base)
+  
+  assets.each do |relative|
+    payload = {
+      :event => "update",
+      :base => base,
+      :relative => relative
+    }
+    puts payload.to_json  
+  end
+  
 end
 
+Listen.to(base_path, :relative_paths => true) do |modified, added, removed|
 
-listener = Listen.to(path)
-listener.change(&callback) # convert the callback to a block and register it
-listener.latency(1)
-listener.start # blocks execution
-
-# FSSM.monitor path do |m|
-#     m.update do |base, relative|
-#       
-#       puts 'Update event'
-#       
-#       unless ignore? relative
-#         puts relative
-#       end
-# 
-#     end
-# 
-#     m.create do |base, relative|
-#       unless ignore? relative
-#         puts relative
-#       end
-#     end
-# 
-#     # if !options['keep_files']
-#     #   m.delete do |base, relative|
-#     #     delete_asset(relative, options['quiet'])          
-#     #   end
-#     # end
-# end
+  # puts 'Modified:'
+  #   modified.each { |item| puts item }
+  #   # puts 'Added:'
+  #   # puts added.inspect
+  
+  broadcast_assets(added, base_path)
+  broadcast_assets(modified, base_path)
+end
