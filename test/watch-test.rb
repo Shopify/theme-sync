9006b9042ed9b7248183796227d296e0ea96644b
Mitchell Amihod <mitchell.amihod@gmail.com>
2013-10-27 18:52:45 -0400

    just testing some new ignores, new bundles

--- test/watch-test.rb
+++ test/watch-test.rb
@@ -2,12 +2,24 @@
 
 require 'socket'
 require 'rubygems'
+
+# To use bundled gems
+root = File.dirname(__FILE__)
+gem_path = File.join(root, '../', 'Resources', 'vendor', 'bundle','ruby','1.8')
+Gem.use_paths(gem_path)
+
+
 require 'json'
-gem 'listen', '= 0.5.2'  
+
+require 'rb-fsevent'
+
+gem 'listen', '= 0.5.3'  
 require 'listen'
+
+
 path = '/Users/mitch/Sites/klocko'
 
-Listen.to(path, :force_polling => false, :ignore => %r[.hg]) do |modified, added, removed|
+Listen.to(path, :force_polling => false, :ignore => %r[.hg|.sass|.less]) do |modified, added, removed|
   puts 'Modified:'
   puts modified.inspect
 end
